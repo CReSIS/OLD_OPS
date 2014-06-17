284924df387b9c108c5058d155679365b3d5790f
Trey Stafford <treystaff@gmail.com>
2014-06-17 11:44:09 -0500

    Integrated into provisions

--- conf/tools/createWeeklyPostgresqlReport.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# SCRIPT FOR CREATING THE WEEKLY POSTGRESQL LOG REPORT
-
-now=$(date +"%Y-%m-%d")
-pgbadger -f stderr -p '%t [%p]: [%l-1] user=%u,db=%d '  -O /cresis/snfs1/web/ops2/postgresql_reports/ -o postgresql_report_$now /cresis/snfs1/web/ops2/pgsql/9.3/pg_log/postgresql-*.log
