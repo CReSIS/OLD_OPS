17326d44aa00385a36401407a652657d9aee6d39
wliu58i5ki <weibo@ku.edu>
2014-11-07 11:44:01 -0600

    update updateGeoserverLayers.sh

--- conf/tools/updateGeoserverLayers.sh
+++ conf/tools/updateGeoserverLayers.sh
@@ -3,11 +3,11 @@
 # UPDATE GEOPORTAL FROM REPO
 
 service tomcat6 stop
-rm -rf /cresis/snfs1/web/ops2/geoserver/styles/*
-cp -rf /vagrant/conf/geoserver/geoserver/styles/* /cresis/snfs1/web/ops2/geoserver/styles/
-rm -rf /cresis/snfs1/web/ops2/geoserver/workspaces/*
-cp -rf /vagrant/conf/geoserver/geoserver/workspaces/* /cresis/snfs1/web/ops2/geoserver/workspaces/
-chmod -R u=rwX,g=rwX,o=rX /cresis/snfs1/web/ops2/geoserver/
-chown -R tomcat:tomcat /cresis/snfs1/web/ops2/geoserver/
+rm -rf /db/geoserver/styles/*
+cp -rf /vagrant/conf/geoserver/geoserver/styles/* /db/geoserver/styles/
+rm -rf /db/geoserver/workspaces/*
+cp -rf /vagrant/conf/geoserver/geoserver/workspaces/* /db/geoserver/workspaces/
+chmod -R u=rwX,g=rwX,o=rX /db/geoserver/
+chown -R tomcat:tomcat /db/geoserver/
 service tomcat6 start
 
