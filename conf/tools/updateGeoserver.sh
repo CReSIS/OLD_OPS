4c26e9d78f5eb757c8a563cc3f02d64411744181
JMColl <JColl@ku.edu>
2016-01-13 10:19:43 -0600

    Update to tools

--- conf/tools/updateGeoserver.sh
+++ conf/tools/updateGeoserver.sh
@@ -3,10 +3,10 @@
 # UPDATE GEOPORTAL FROM REPO
 
 service tomcat6 stop
-mv /cresis/snfs1/web/ops2/geoserver/data /tmp/data
-rm -rf /cresis/snfs1/web/ops2/geoserver/*
-cp -rf /vagrant/conf/geoserver/geoserver/* /cresis/snfs1/web/ops2/geoserver/
-mv /tmp/data /cresis/snfs1/web/ops2/geoserver/data
-chmod -R u=rwX,g=rwX,o=rX /cresis/snfs1/web/ops2/geoserver/
-chown -R tomcat:tomcat /cresis/snfs1/web/ops2/geoserver/
+mv /var/lib/tomcat6/webapps/geoserver/data /tmp/data
+rm -rf /var/lib/tomcat6/webapps/geoserver/*
+cp -rf /vagrant/conf/geoserver/* /var/lib/tomcat6/webapps/geoserver/
+mv /tmp/data /var/lib/tomcat6/webapps/geoserver/data
+chmod -R u=rwX,g=rwX,o=rX /var/lib/tomcat6/webapps/geoserver/
+chown -R tomcat:tomcat /var/lib/tomcat6/webapps/geoserver
 service tomcat6 start
