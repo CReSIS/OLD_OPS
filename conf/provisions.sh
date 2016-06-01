97e42e174cb7d526be88e24826cea5c499fbf8c6
JMColl <Jamesmcoll@gmail.com>
2016-06-01 18:16:14 -0500

    Update provisions.sh

--- conf/provisions.sh
+++ conf/provisions.sh
@@ -478,7 +478,7 @@ yum -y install geos-devel
 # INSTALL AND CONFIGURE DJANGO
 
 # INSTALL DJANGO
-pip install Django==1.6.5
+pip install Django==1.8.2
 
 # CREATE DIRECTORY AND COPY PROJECT
 mkdir -p /var/django/
