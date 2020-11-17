64e0e29843cc7102cac96adc486f9d6870cd958e
Reece Mathews <mathewsreece@gmail.com>
2020-11-17 01:41:46 -0600

    Add on_delete param to ForeignKeys

--- conf/django/ops/kuband/models.py
+++ conf/django/ops/kuband/models.py
@@ -11,8 +11,8 @@ class locations(models.Model):
 
 class seasons(models.Model):
 
-    location = models.ForeignKey("locations")
-    season_group = models.ForeignKey("season_groups")
+    location = models.ForeignKey("locations", on_delete=models.CASCADE)
+    season_group = models.ForeignKey("season_groups", on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
 
     def __unicode__(self):
@@ -39,8 +39,8 @@ class radars(models.Model):
 
 class segments(models.Model):
 
-    season = models.ForeignKey("seasons")
-    radar = models.ForeignKey("radars")
+    season = models.ForeignKey("seasons", on_delete=models.CASCADE)
+    radar = models.ForeignKey("radars", on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     geom = models.LineStringField()
     objects = models.GeoManager()
@@ -51,7 +51,7 @@ class segments(models.Model):
 
 class frames(models.Model):
 
-    segment = models.ForeignKey("segments")
+    segment = models.ForeignKey("segments", on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
 
     def __unicode__(self):
@@ -60,10 +60,10 @@ class frames(models.Model):
 
 class point_paths(models.Model):
 
-    location = models.ForeignKey("locations")
-    season = models.ForeignKey("seasons")
-    segment = models.ForeignKey("segments")
-    frame = models.ForeignKey("frames")
+    location = models.ForeignKey("locations", on_delete=models.CASCADE)
+    season = models.ForeignKey("seasons", on_delete=models.CASCADE)
+    segment = models.ForeignKey("segments", on_delete=models.CASCADE)
+    frame = models.ForeignKey("frames", on_delete=models.CASCADE)
     gps_time = models.DecimalField(max_digits=16, decimal_places=6, db_index=True)
     roll = models.DecimalField(max_digits=6, decimal_places=5)
     pitch = models.DecimalField(max_digits=6, decimal_places=5)
@@ -78,10 +78,10 @@ class point_paths(models.Model):
 class crossovers(models.Model):
 
     point_path_1 = models.ForeignKey(
-        "point_paths", related_name="point_paths_link_fk_1"
+        "point_paths", related_name="point_paths_link_fk_1", on_delete=models.CASCADE
     )
     point_path_2 = models.ForeignKey(
-        "point_paths", related_name="point_paths_link_fk_2"
+        "point_paths", related_name="point_paths_link_fk_2", on_delete=models.CASCADE
     )
     angle = models.DecimalField(max_digits=6, decimal_places=3)
     geom = models.PointField()
@@ -104,7 +104,7 @@ class layer_groups(models.Model):
 class layers(models.Model):
 
     id = models.AutoField(primary_key=True)
-    layer_group = models.ForeignKey("layer_groups")
+    layer_group = models.ForeignKey("layer_groups", on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200, blank=True)
     deleted = models.BooleanField(default=False)
@@ -115,8 +115,8 @@ class layers(models.Model):
 
 class layer_links(models.Model):
 
-    layer_1 = models.ForeignKey("layers", related_name="layer_link_fk_1")
-    layer_2 = models.ForeignKey("layers", related_name="layer_link_fk_2")
+    layer_1 = models.ForeignKey("layers", related_name="layer_link_fk_1", on_delete=models.CASCADE)
+    layer_2 = models.ForeignKey("layers", related_name="layer_link_fk_2", on_delete=models.CASCADE)
 
     def __unicode__(self):
         return "%d" % (self.id)
@@ -127,12 +127,12 @@ class layer_points(models.Model):
     # import User for 'user_id' column
     from django.contrib.auth.models import User
 
-    layer = models.ForeignKey("layers")
-    point_path = models.ForeignKey("point_paths")
+    layer = models.ForeignKey("layers", on_delete=models.CASCADE)
+    point_path = models.ForeignKey("point_paths", on_delete=models.CASCADE)
     twtt = models.DecimalField(max_digits=12, decimal_places=11, blank=True, null=True)
     type = models.IntegerField(blank=True, null=True)  # 1:manual,2:auto
     quality = models.IntegerField(blank=True, null=True)  # 1:good,2:moderate,3:derived
-    user = models.ForeignKey(User, related_name="kuband_user_id_fk")
+    user = models.ForeignKey(User, related_name="kuband_user_id_fk", on_delete=models.CASCADE)
     last_updated = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -144,13 +144,13 @@ class layer_points(models.Model):
 
 class landmarks(models.Model):
 
-    radar = models.ForeignKey("radars")
-    segment = models.ForeignKey("segments")
+    radar = models.ForeignKey("radars", on_delete=models.CASCADE)
+    segment = models.ForeignKey("segments", on_delete=models.CASCADE)
     point_path_1 = models.ForeignKey(
-        "point_paths", related_name="point_paths_link_fk2_1"
+        "point_paths", related_name="point_paths_link_fk2_1", on_delete=models.CASCADE
     )
     point_path_2 = models.ForeignKey(
-        "point_paths", related_name="point_paths_link_fk2_2"
+        "point_paths", related_name="point_paths_link_fk2_2", on_delete=models.CASCADE
     )
     start_twtt = models.DecimalField(max_digits=12, decimal_places=11, db_index=True)
     stop_twtt = models.DecimalField(max_digits=12, decimal_places=11, db_index=True)
