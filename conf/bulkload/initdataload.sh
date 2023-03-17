760fc9f3b52dc480908631d7e15a25a3852cb12f
Reece Mathews <mathewsreece@ku.edu>
2023-03-17 14:46:09 -0500

    Automatically reformat datapacks from older versions of OPS

--- conf/bulkload/initdataload.sh
+++ conf/bulkload/initdataload.sh
@@ -11,7 +11,7 @@ pgconfdir=$pgdir"postgresql.conf"
 
 #MODIFY POSTGRESQL.CONF
 printf "${STATUS_COLOR}Updating postgresql.conf${NC}\n";
-# sed -i "s,#checkpoint_segments = 3,checkpoint_segments = 100,g" $pgconfdir  # This setting has been deprecated for (min/)max_wal_size 
+# sed -i "s,#checkpoint_segments = 3,checkpoint_segments = 100,g" $pgconfdir  # This setting has been deprecated for (min/)max_wal_size
 sed -i "s,#checkpoint_timeout = 5min,checkpoint_timeout = 1h,g" $pgconfdir
 sed -i "s,#checkpoint_completion_target = 0.5,checkpoint_completion_target = 0.9,g" $pgconfdir
 
@@ -24,23 +24,49 @@ sleep 2
 printf "${STATUS_COLOR}Preparing db for dataload${NC}\n";
 psql -U postgres -d ops -c "SET maintenance_work_mem TO '1024MB';"
 
-#Make a temporary directory and unpack initial data files to it. 
+#Make a temporary directory and unpack initial data files to it.
 printf "${STATUS_COLOR}Creating temp dir${NC}\n";
 mkdir /tmp/pgdata/;
 
-#Find all selected initial data files then unpack and load 
+#Find all selected initial data files then unpack and load
 printf "${STATUS_COLOR}Finding, unpacking, and unloading data files${NC}\n";
 (
 cd /opt/ops/data/postgresql/
 for pack in *.tar.gz
-do 
+do
     printf "${STATUS_COLOR}Unpacking ${pack}${NC}\n";
 	tar -zxf /opt/ops/data/postgresql/$pack -C /tmp/pgdata/;
-	#Use pg_bulkload to load initial data into the database. 
+	#Use pg_bulkload to load initial data into the database.
 	cd /tmp/pgdata/
     printf "${STATUS_COLOR}Loading files from ${pack}${NC}\n";
-	for file in *; 
-	do 
+
+	# Make corrections to datapacks if they are from an old version of OPS
+	for file in *_segments;
+	do
+		# Check if file has expected 6 columns or is missing the crossover_calc column (as with older versions of OPS)
+		num_commas=`head -n 1 $file | grep -o ',' | wc -l`
+		if [ "$num_commas" -lt 5 ]; then
+			printf "${STATUS_COLOR}Adding default to crossover_calc to ${file} from ${pack}${NC}\n";
+			awk -F"," 'BEGIN { OFS = "," } {$6="true"; print}' $file > "new_${file}"
+			rm -f $file;
+			mv "new_${file}" $file
+		fi
+	done
+	for file in *_point_paths;
+	do
+		# Check if file has expected 10 columns or is missing the key_point column (as with older versions of OPS)
+		num_commas=`head -n 1 $file | grep -o ',' | wc -l`
+		if [ "$num_commas" -lt 10 ]; then
+			printf "${STATUS_COLOR}Adding default to key_point to ${file} from ${pack}${NC}\n";
+			awk -F"," 'BEGIN { OFS = "," } {$11="true"; print}' $file > "new_${file}"
+			rm -f $file;
+			mv "new_${file}" $file
+		fi
+	done
+
+	# Load the files
+	for file in *;
+	do
         printf "${STATUS_COLOR}Loading ${file} from ${pack}${NC}\n";
 		/usr/pgsql-12/bin/pg_bulkload -d ops -U admin -i /tmp/pgdata/$file -O $file;
 		rm -f $file;
@@ -50,7 +76,7 @@ done
 printf "${STATUS_COLOR}Removing temp dir${NC}\n";
 rmdir /tmp/pgdata/;
 
-# CALL SQL TO RESUME NORMAL DATABASE 
+# CALL SQL TO RESUME NORMAL DATABASE
 printf "${STATUS_COLOR}Cleanup bulkload${NC}\n";
 su - postgres -c "psql -d ops -f /opt/ops/conf/bulkload/pg_bulkload_cleanup.sql"
 
